78e0cf59c9944c2319832ec984304dd07810d49f
gitsibi <159125779+gitsibi@users.noreply.github.com>
2024-04-22 12:35:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,18 +23,4 @@
 //   const c3 = document.getElementById('react-container');
 //   ReactDOM.render(React.createElement(ReactContainer),c3)
 
-//      -------babel-----
-// class ReactContainer extends React.Component {
-//     render() {
-//       return (
-//         <div>
-//           Hello! Welcome to Kalvium   
-//             <div>            
-//                Let's rock and roll - this is babel
-//             </div>
-//         </div>
-//       );
-//     }  
-//   }
-//   const c4 = document.getElementById('react-container');
-//   ReactDOM.render(<ReactContainer/>, c4); 
+
